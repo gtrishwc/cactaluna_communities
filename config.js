7aea6b6c0365461a91ecad761bb6ffc1bcc20317
gtrishwc <cactaluna.studio@gmail.com>
2026-07-16 14:09:19 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
-
+const CONFIG = {
+  nombre: "Real de Palmas",
+  logo: "img/logo.png",
+  colorPrincipal: "#1F6F5B",
+  colorSecundario: "#2D8F78",
+  telefonoEmergencias: "662-000-0000",
+  whatsappComite: "526621234567"
+};
